4691c5701734d4daa40de562b4f0e0df0e9ab541
Dani Pastor <danpassol@alu.edu.gva.es>
2025-04-25 09:27:24 +0200

    removed if statements

--- core/distro.py
+++ core/distro.py
@@ -26,59 +26,54 @@ class Distro:
                 elif any(line.startswith(prefix) for prefix in ("VERSION_CODENAME=", "VERSION=")) and not self.version_codename:
                     self.version_codename = line.split("=")[1].strip().strip('"')
 
-        if self.id in ["ubuntu", "debian"]:
-            self.package_manager = "apt"
-        elif self.id in ["rhel", "fedora", "centos"]:
-            self.package_manager = "dnf"
-        elif self.id in ["arch"]:
-            self.package_manager = "pacman"
-        elif self.id in ["alpine"]:
-            self.package_manager = "apk"
-        elif self.id in ["gentoo"]:
-            self.package_manager = "emerge"
-        elif re.match(r"^opensuse", self.id):
-            self.package_manager = "zypper"
-        else:
-            raise Exception(f"Unsupported distro: {self.id}")
+                # use of dictionary to map the ID to a package manager instead of multiple if-elif statements
+        package_managers = {
+            "apt": ["ubuntu", "debian"],
+            "dnf": ["rhel", "fedora", "centos"],
+            "pacman": ["arch"],
+            "apk": ["alpine"],
+            "emerge": ["gentoo"],
+            "zypper": [r"^opensuse"]  # regex pattern for anything starting with opensuse, this is because openSUSE has multiple versions like Tumbleweed, Leap, etc.
+        }
+
+        for manager, ids in package_managers.items():
+            for distro_id in ids:
+                if distro_id.startswith("^"):  # regex match
+                    if re.match(distro_id, self.id):
+                        self.package_manager = manager
+                        return
+                elif self.id == distro_id:
+                    self.package_manager = manager
+                    return
+
+        raise Exception(f"Unsupported distro: {self.id}")
 
     def install_cmd(self, packages):
-        if self.package_manager == "apt":
-            return f"apt install -y {' '.join(packages)}"
-        elif self.package_manager == "dnf":
-            return f"dnf install -y {' '.join(packages)}"
-        elif self.package_manager == "pacman":
-            return f"pacman -S --noconfirm {' '.join(packages)}"
-        elif self.package_manager == "apk":
-            return f"apk add {' '.join(packages)}"
-        elif self.package_manager == "emerge":
-            return f"emerge {' '.join(packages)}"
-        elif self.package_manager == "zypper":
-            return f"zypper install -y {' '.join(packages)}"
+        return {
+            "apt": f"apt install -y {' '.join(packages)}",
+            "dnf": f"dnf install -y {' '.join(packages)}",
+            "pacman": f"pacman -S --noconfirm {' '.join(packages)}",
+            "apk": f"apk add {' '.join(packages)}",
+            "emerge": f"emerge {' '.join(packages)}",
+            "zypper": f"zypper install -y {' '.join(packages)}",
+        }[self.package_manager]
 
     def update_cmd(self):
-        if self.package_manager == "apt":
-            return "apt update && apt upgrade -y"
-        elif self.package_manager == "dnf":
-            return "dnf upgrade --refresh -y"
-        elif self.package_manager == "pacman":
-            return "pacman -Syu --noconfirm"
-        elif self.package_manager == "apk":
-            return "apk update && apk upgrade"
-        elif self.package_manager == "emerge":
-            return "emerge --sync && emerge --update --deep --newuse @world"
-        elif self.package_manager == "zypper":
-            return "zypper refresh && zypper update -y"
+        return {
+            "apt": "apt update && apt upgrade -y",
+            "dnf": "dnf upgrade --refresh -y",
+            "pacman": "pacman -Syu --noconfirm",
+            "apk": "apk update && apk upgrade",
+            "emerge": "emerge --sync && emerge --update --deep --newuse @world",
+            "zypper": "zypper refresh && zypper update -y",
+        }[self.package_manager]
 
     def remove_cmd(self, packages):
-        if self.package_manager == "apt":
-            return f"apt purge -y {' '.join(packages)}"
-        elif self.package_manager == "dnf":
-            return f"dnf remove -y {' '.join(packages)}"
-        elif self.package_manager == "pacman":
-            return f"pacman -R --noconfirm {' '.join(packages)}"
-        elif self.package_manager == "apk":
-            return f"apk del {' '.join(packages)}"
-        elif self.package_manager == "emerge":
-            return f"emerge --unmerge {' '.join(packages)}"
-        elif self.package_manager == "zypper":
-            return f"zypper remove -y {' '.join(packages)}"
+        return {
+            "apt": f"apt purge -y {' '.join(packages)}",
+            "dnf": f"dnf remove -y {' '.join(packages)}",
+            "pacman": f"pacman -R --noconfirm {' '.join(packages)}",
+            "apk": f"apk del {' '.join(packages)}",
+            "emerge": f"emerge --unmerge {' '.join(packages)}",
+            "zypper": f"zypper remove -y {' '.join(packages)}",
+        }[self.package_manager]
