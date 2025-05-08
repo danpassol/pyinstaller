4afcb73e9fa396a59de12df49423c9a8e373d5f1
Dani Pastor <danpassol@alu.edu.gva.es>
2025-05-08 12:15:43 +0200

    added animations and requirements

--- core/runner.py
+++ core/runner.py
@@ -3,8 +3,10 @@
 import subprocess
 from core.distro import Distro
 from core.logger import setup_logger
+from rich.console import Console
 
 log = setup_logger()
+console = Console()
 
 class CommandRunner:
     def __init__(self, verbose=False):
@@ -12,7 +14,7 @@ class CommandRunner:
         self.distro = Distro()  # Uses the singleton instance
         self.package_manager = self.distro.package_manager
 
-    def _run_cmd(self, cmd, capture_output=False):
+    def _run_cmd(self, cmd, capture_output=False, spinner=None, description="Processing command..."):
         try:
             if self.verbose:
                 process = subprocess.Popen(cmd, shell=True)
@@ -20,30 +22,36 @@ class CommandRunner:
                 if process.returncode != 0:
                     log.error(f"Command failed: {cmd}")
                 return None
+
+            if spinner:
+                with console.status(f"[bold green]{description}", spinner=spinner):
+                    result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             else:
                 result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                if capture_output:
-                    return result.stdout.decode().strip()
-                return None
+
+            if capture_output:
+                return result.stdout.decode().strip()
+            return None
+
         except subprocess.CalledProcessError as e:
             log.error(f"Command failed: {cmd}")
             log.error(f"Error message: {e.stderr.decode().strip()}")
             raise e
         
-    def install(self, packages):
+    def install(self, packages, spinner="dots",):
         cmd = self.distro.install_cmd(packages)
         log.info(f"Installing packages: {packages}")
-        return self._run_cmd(cmd)
+        return self._run_cmd(cmd, spinner=spinner, description="Installing...")
 
-    def upgrade(self):
+    def upgrade(self, spinner="dots",):
         cmd = self.distro.update_cmd()
         log.info("Upgrading system...")
-        return self._run_cmd(cmd)
+        return self._run_cmd(cmd, spinner=spinner, description="Updating...")
 
-    def remove(self, packages):
+    def remove(self, packages, spinner="dots",):
         cmd = self.distro.remove_cmd(packages)
         log.info(f"Removing packages: {packages}")
-        return self._run_cmd(cmd)
+        return self._run_cmd(cmd, spinner=spinner, description="Removing...")
 
-    def run(self, cmd, capture_output=False):
-        return self._run_cmd(cmd, capture_output)
+    def run(self, cmd, capture_output=False, spinner=None):
+        return self._run_cmd(cmd, capture_output, spinner=spinner, description=None)
